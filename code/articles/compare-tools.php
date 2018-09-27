1560fc898f259cd2a5615a7cc4f5f2affa462a7a
Kristopher Windsor <kristopher@pinger.com>
2018-09-26 23:44:36 -0700

    Add "compare tools" page

---
+++ code/articles/compare-tools.php
@@ -0,0 +1,45 @@
+<article>
+    <p>
+        This table lets you compare the major features of each static analysis tool.
+        Another good way to compare tools is with the live <a href="/demo">demo</a>.
+    </p>
+
+    <table id="comparetools">
+        <thead>
+            <tr>
+                <th>Tool</th>
+                <th>Checks code complexity / risk</th>
+                <th>Checks code style: whitespace, etc.</th>
+                <th>Checks code style: good variable names, etc.</th>
+                <th>Checks code for bugs</th>
+                <th>Provides code metrics</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><a href="/intro-to-phpcs">PHP_CodeSniffer</a></td>
+                <td></td>
+                <td class="yes">Yes</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><a href="/intro-to-phpmd">PHP Mess Detector</a></td>
+                <td class="yes">Yes</td>
+                <td></td>
+                <td class="yes">Yes</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><a href="/intro-to-php-metrics">PhpMetrics</a></td>
+                <td class="yes">Yes</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="yes">Yes (HTML report)</td>
+            </tr>
+        </tbody>
+    </table>
+</article>
